7ac694726070ae210b5e041e221732a463339a50
Orosz Levente János <Olevente@gmail.com>
2022-12-27 18:28:12 +0100

    Done version +0.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+ /*
+* File: app.routing.ts
+* Author: Orosz Levente János
+* Copyright: 2022, Orosz Levente János
+* Group: Szoft II N
+* Date: 2022-12-28
+* Github: https://github.com/Olevente
+* Licenc: GNU GPL
+*/ 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DataComponent } from './data/data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "data", component:DataComponent},
+  {path : "about", component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,3 +1,13 @@
+ /*
+* File: data.component.ts
+* Author: Orosz Levente János
+* Copyright: 2022, Orosz Levente János
+* Group: Szoft II N
+* Date: 2022-12-28
+* Github: https://github.com/Olevente
+* Licenc: GNU GPL
+*/ 
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +17,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
+ 
+;
+place: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.place = [
+      {name: "Székesfehérvár", county: "Fejér", area: 170.89, population: 95545},
+      {name: "Szombathely", county: "Vas", area: 97.5, population:	78324},
+      {name: "Sopron", county: "Győr-Moson-Sopron", area: 169.04, population: 62900},
+      {name: "Zalaegerszeg", county: "Zala", area: 102.42, population: 55470},
+      {name: "Sárvár", county: "Vas", area:	64.65, population: 15072},
+      {name: "Lenti", county: "Zala", area:	73.79, population: 7348},
+      {name: "Zalakaros", county: "Zala", area: 17.17, population: 1936}
+    ]
+
+   
+    
   }
 
 }
